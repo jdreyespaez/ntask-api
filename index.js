804872d778c6e050dfc3ea0ced8275e4bf84a24e
jdreyespaez <jd.reyespaez@gmail.com>
2016-04-06 17:56:57 +0700

    Setting up Sequelize

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 import express from "express";
+import consign from "consign";
 
-const PORT = 3000;
+// const PORT = 3000;
 const app = express();
 
-app.set("json spaces", 4);
+consign()
+  .include("db.js")
+  .then("models")
+  .then("libs/middlewares.js")
+  .then("routes")
+  .then("libs/boot.js")
+  .into(app);
+//
+// app.get("/", (req,res) => res.json({status: "NTask API"}));
+//
+// app.get("/tasks", (req,res) => {
+//   res.json({
+//     tasks: [
+//       {title: "Buy some shoes"},
+//       {title: "Fix notebook"}
+//     ]
+//   });
+// });
 
-app.get("/", (req,res) => res.json({status: "NTask API"}));
-
-app.get("/tasks", (req,res) => {
-  res.json({
-    tasks: [
-      {title: "Buy some shoes"},
-      {title: "Fix notebook"}
-    ]
-  });
-});
-
-app.listen(PORT, () => console.log(`NTask API - Port ${PORT}`));
+// app.listen(PORT, () => console.log(`NTask API - Port ${PORT}`));
