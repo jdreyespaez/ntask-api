ef61e067bfdbf1ffca4782dda4cca043ca32a7a6
jdreyespaez <jd.reyespaez@gmail.com>
2016-04-09 20:17:09 +0700

    Documenting token generation

--- routes/token.js
+++ routes/token.js
@@ -3,6 +3,23 @@ import jwt from "jwt-simple";
 module.exports = app => {
   const cfg = app.libs.config;
   const Users = app.db.models.Users;
+
+  /**
+    * @api {post} /token Authentication Token
+    * @apiGroup Credentials
+    * @apiParam {String} email User email
+    * @apiParam {String} password User password
+    * @apiParamExample {json} Input
+    *   {
+    *     "email": "john@connor.net",
+    *     "password": "123456"
+    *   }
+    * @apiSuccess {String} token Token of authenticated user
+    * @apiSuccessExample {json} Success
+    *   HTTP/1.1 200 OK
+    *   {"token": "xyz.abc.123.hgf"}
+  */
+
   app.post("/token", (req, res) => {
     if (req.body.email && req.body.password) {
       const email = req.body.email;
